77692737e5e6bd8448bdb59309c980ad39f35ee5
velkoSimarro <velko_bg@abv.bg>
2015-02-13 20:06:38 +0100

    Test

--- temp/templates_c/c_e891d65ab5bb70321e9145d4b717426f.php
+++ temp/templates_c/c_e891d65ab5bb70321e9145d4b717426f.php
@@ -1,3 +1,27 @@
+<?php /* 1.0 2015-02-13 20:04:12 CET */ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title><?php echo $this->_vars['site']['title']; ?>
+</title>
+		<?php echo '
+		<link rel="stylesheet" href="';  echo $this->_vars['site']['style'];  echo 'style.css" />
+		'; ?>
+
+	</head>
+	<body>
+		<div class="title">
+			<img src="<?php echo $this->_vars['site']['img']; ?>
+core.png" alt="" /><br />
+			<?php echo $this->_vars['name']; ?>
+ <?php echo $this->_vars['version']; ?>
+ !<br />
+			<a href="javascript:;" title="" onclick="Core.hello();" />Version</a>
+		</div>
+
+		<pre>
 The MIT License (MIT)
 
 Copyright (c) 2015 Velik Georgiev Chelebiev
@@ -18,4 +42,14 @@ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE..
+THE SOFTWARE.
+		</pre>
+		
+		<?php echo '
+		<script src="';  echo $this->_vars['site']['script'];  echo 'jquery.min.js" type="text/javascript"></script>
+		<script src="';  echo $this->_vars['site']['script'];  echo 'config.js" type="text/javascript"></script>
+		<script src="';  echo $this->_vars['site']['script'];  echo 'main.js" type="text/javascript"></script>
+		'; ?>
+
+	</body>
+</html>
